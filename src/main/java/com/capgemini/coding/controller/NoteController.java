96175b7b33a7c56b71c7830b8ab2299bf9565cf2
Wensveen <raymond@brackets.ninja>
2018-06-25 11:45:49 +0200

    added relationship

--- src/main/java/com/capgemini/coding/controller/NoteController.java
+++ src/main/java/com/capgemini/coding/controller/NoteController.java
@@ -2,8 +2,11 @@ package com.capgemini.coding.controller;
 
 import com.capgemini.coding.exception.ResourceNotFoundException;
 import com.capgemini.coding.model.Note;
+import com.capgemini.coding.repository.CategoryRepository;
 import com.capgemini.coding.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -15,6 +18,52 @@ public class NoteController {
 
     @Autowired
     NoteRepository noteRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @GetMapping("/categories/{id}/notes")
+    public Page<Note> getAllNotesByCategoryId(@PathVariable Long id,
+                                              Pageable pageable) {
+        return noteRepository.findByCategoryId(id, pageable);
+    }
+
+    @PostMapping("/categories/{id}/notes")
+    public Note createNote(@PathVariable Long id, @Valid @RequestBody Note note){
+        return categoryRepository.findById(id).map(category -> {
+            note.setCategory(category);
+            return noteRepository.save(note);
+
+        }).orElseThrow(() -> new ResourceNotFoundException("Category", "id", id));
+    }
+
+    @PutMapping("/categories/{categoryId}/notes/{noteId}")
+    public Note updateNote(@PathVariable (value= "categoryId") Long categoryId,
+                           @PathVariable (value = "noteId") Long noteId,
+                           @Valid @RequestBody Note noteRequest) {
+        if(!categoryRepository.existsById(categoryId)) {
+            throw new ResourceNotFoundException("Category", "id", categoryId);
+        }
+
+        return noteRepository.findById(noteId).map(note -> {
+            note.setTitle(noteRequest.getTitle());
+            note.setContent(noteRequest.getContent());
+            return noteRepository.save(note);
+        }).orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+    }
+
+    @DeleteMapping("/categories/{categoryId}/notes/{noteId}")
+    public ResponseEntity<?>  deleteNote(@PathVariable (value = "categoryId") Long categoryId,
+                                         @PathVariable (value = "noteId") Long noteId) {
+        if(!categoryRepository.existsById(categoryId)) {
+            throw new ResourceNotFoundException("Category", "id", categoryId);
+        }
+
+        return noteRepository.findById(noteId).map(note -> {
+            noteRepository.delete(note);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+    }
 
     // Get All Notes
     @GetMapping("/notes")
--- src/main/java/com/capgemini/coding/model/Note.java
+++ src/main/java/com/capgemini/coding/model/Note.java
@@ -1,40 +1,31 @@
 package com.capgemini.coding.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import java.io.Serializable;
-import java.util.Date;
+
 
 @Entity
 @Table(name = "notes")
-@EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
-        allowGetters = true)
-public class Note implements Serializable {
+public class Note extends AuditModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Category category;
 
     @NotBlank
     private String title;
 
     @NotBlank
     private String content;
-
-    @Column(nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    private Date updatedAt;
 
     public Long getId() {
         return id;
@@ -60,20 +51,12 @@ public class Note implements Serializable {
         this.content = content;
     }
 
-    public Date getCreatedAt() {
-        return createdAt;
+
+    public Category getCategory() {
+        return category;
     }
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
+    public void setCategory(Category category) {
+        this.category = category;
     }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
 }
